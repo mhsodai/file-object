10152c5e4f41b267d4a9045ec6d0bef2856f6ed9
mh.sodai <mh.sodai@gmail.com>
2017-09-15 01:16:52 +0900

    Create project and source code, Change readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # file-object
 Object-oriented library, for easy file move, copy, rename and delete
+
+# Examples
+If you move "C:\foo\hello.txt" to "C:\bar\"
+```
+var srcFile = @"C:\foo\hello.txt";
+var dstDir = @"C:\bar\";
+
+// use System.IO.File
+File.Move(srcFile, Path.Combine(dstDir, Path.GetFileName(srcFile)));
+
+// use this library
+srcFile.ToFileObject()
+  .MoveToDirectory(dstDir);
+```
+
+If you copy "C:\foo\hello.txt" to "D:\bar\",
+but first copied "hello._txt" then rename "hello.txt"
+
+```
+var srcFile = @"C:\foo\hello.txt";
+var dstDir = @"C:\bar\";
+
+// use System.IO.File
+var srcFileName = Path.GetFileName(srcFile);
+var tempFileName = Path.ChangeExtension(srcFileName, "._txt");
+File.Copy(srcFile, Path.Combine(dstDir, tempFileName));
+File.Move(Path.Combine(dstDir, tempFileName), Path.Combine(dstDir, srcFileName));
+
+// use this library
+srcFile.ToFileObject()
+  .CopyToDirectory(dstDir, rename: fileName => Path.ChangeExtension(fileName, "_txt"))
+  .ChangeExtension("txt");
+```
---
+++ file-object/FileObject.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+
+namespace FileObject
+{
+    public class FileObject
+    {
+        private string _fullPath;
+
+        public string FileName { get { return Path.GetFileName(_fullPath); } }
+
+        public FileObject(string path)
+        {
+            _fullPath = path;
+        }
+
+        public void MoveTo(string path, bool overwrite = false)
+        {
+            if (overwrite && File.Exists(path))
+                File.Delete(path);
+
+            File.Move(_fullPath, path);
+            _fullPath = path;
+        }
+
+        public void MoveToDirectory(string folderPath, Func<string, string> rename = null, bool overwrite = false)
+        {
+            var path = Path.Combine(folderPath, rename != null ? rename(FileName) : FileName);
+            if (overwrite && File.Exists(path))
+                File.Delete(path);
+
+            File.Move(_fullPath, path);
+            _fullPath = path;
+        }
+
+        public FileObject CopyTo(string path, bool overwrite = false)
+        {
+            File.Copy(_fullPath, path, overwrite);
+            return path.ToFileObject();
+        }
+
+        public FileObject CopyToDirectory(string folderPath, Func<string, string> rename = null, bool overwrite = false)
+        {
+            var path = Path.Combine(folderPath, rename != null ? rename(FileName) : FileName);
+            File.Copy(_fullPath, path, overwrite);
+            return path.ToFileObject();
+        }
+
+        public void Rename(Func<string, string> rename)
+        {
+            var folderPath = Path.GetDirectoryName(_fullPath);
+            var path = Path.Combine(folderPath, rename != null ? rename(FileName) : FileName);
+            File.Move(_fullPath, path);
+            _fullPath = path;
+        }
+
+        public void ChangeExtension(Func<string, string> changeExtension)
+        {
+            var extension = Path.GetExtension(_fullPath);
+            var path = Path.ChangeExtension(_fullPath, changeExtension(extension));
+            File.Move(_fullPath, path);
+            _fullPath = path;
+        }
+
+        public void Delete()
+        {
+            File.Delete(_fullPath);
+        }
+
+        public bool Exists() => File.Exists(_fullPath);
+        public void Rename(string fileName) => Rename(_ => fileName);
+        public void ChangeExtension(string extension) => ChangeExtension(_ => extension);
+    }
+
+    public static class FileObjectExtensionMethods
+    {
+        public static FileObject ToFileObject(this string path) => new FileObject(path);
+    }
+}
